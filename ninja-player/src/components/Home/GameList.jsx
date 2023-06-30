1f2f1f2ea847c52a79ebfbd3f2586dda88711c2a
Parth Bisht <bishtparth795@gmail.com>
2023-06-30 19:52:31 +0530

    firebase integration

--- ninja-player/src/components/Home/GameList.jsx
+++ ninja-player/src/components/Home/GameList.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Data from "@/shared/Data";
 
 const GameList = () => {
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    setGames(Data.GameList);
+  }, []);
+  console.log(games);
   return (
-    <div>
-      GameList
+    <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 mt-4">
+      {games.map((item) => (
+        <div className="flex flex-col items-center cursor-pointer">
+          <img
+            src={item.image}
+            alt="game"
+            width={45}
+            height={45}
+            className="hover:animate-bounce transition-all duration-150"
+          />
+          <h2 className="text-[14px] text-center">{item.name}</h2>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default GameList
+export default GameList;
--- ninja-player/src/pages/index.js
+++ ninja-player/src/pages/index.js
@@ -1,14 +1,27 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import Hero from "@/components/Home/Hero";
 import Search from "@/components/Home/Search";
 import GameList from "@/components/Home/GameList";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import app from "@/shared/FirebaseConfig";
 import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
+  const getPost = async (db) => {
+    const querySnapshot = await getDocs(collection(db, "posts"));
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      console.log(window);
+      const db = getFirestore(app);
+      getPost(db);
+    }
+  }, []);
   return (
     <div className="px-5 sm:px-7 md:px-10 mt-9">
       <Hero />
